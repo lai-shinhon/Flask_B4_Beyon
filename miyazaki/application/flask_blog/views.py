18d372824ca5811968f7d4b77c46615b2ec2e77a
TakahiroMiyazai <miyazaki.takahiro.bg@mynavi.jp>
2024-05-28 01:29:44 +0000

    Up to chapter 17

--- miyazaki/application/flask_blog/views.py
+++ miyazaki/application/flask_blog/views.py
@@ -2,9 +2,11 @@ from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
 
 
+
 @app.route('/')
 def show_entries():
     return render_template('entries/index.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
