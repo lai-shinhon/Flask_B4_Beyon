2a29e8bf903437d6b66828f259d1f77c3eb66442
lai-shinhon <lai.xinhong.au@mynavi.jp>
2024-05-29 07:51:07 +0000

    Update to chapter 27

--- shinhon/application/flask_blog/views/views.py
+++ shinhon/application/flask_blog/views/views.py
@@ -1,12 +1,12 @@
 from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
 
-@app.route('/')
-def show_entries():
-    print(session.get('logged_in'))
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
-    return render_template('entries/index.html')
+# @app.route('/')
+# def show_entries():
+#     print(session.get('logged_in'))
+#     if not session.get('logged_in'):
+#         return redirect(url_for('login'))
+#     return render_template('entries/index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
