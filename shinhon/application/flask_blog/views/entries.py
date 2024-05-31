ab53cd3b5d985752735f5f2705f74c957d668d5b
lai-shinhon <lai.xinhong.au@mynavi.jp>
2024-05-31 01:49:45 +0000

    Update to chapter 31

--- shinhon/application/flask_blog/views/entries.py
+++ shinhon/application/flask_blog/views/entries.py
@@ -2,25 +2,23 @@ from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
 from flask_blog import db
 from flask_blog.models.entries import Entry
+from flask_blog.views.views import login_required
 
 @app.route('/')
+@login_required
 def show_entries():
     print(session.get('logged_in'))
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
     entries = Entry.query.all()
     return render_template('entries/index.html', entries=entries)
 
 @app.route('/entries/new', methods=['GET'])
+@login_required
 def new_entry():
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
     return render_template('entries/new.html')
 
 @app.route('/entries', methods=['POST'])
+@login_required
 def add_entry():
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
     entry = Entry(
         title=request.form['title'],
         text=request.form['text']
@@ -31,23 +29,20 @@ def add_entry():
     return redirect(url_for('show_entries'))
 
 @app.route('/entries/<int:id>', methods=['GET'])
+@login_required
 def show_entry(id):
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
     entry = Entry.query.get(id)
     return render_template('entries/show.html', entry=entry)
 
 @app.route('/entries/<int:id>/edit', methods=['GET'])
+@login_required
 def edit_entry(id):
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
     entry = Entry.query.get(id)
     return render_template('entries/edit.html', entry=entry)
 
 @app.route('/entries/<int:id>/update', methods=['POST'])
+@login_required
 def update_entry(id):
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
     entry = Entry.query.get(id)
     entry.title = request.form['title']
     entry.text = request.form['text']
@@ -57,9 +52,8 @@ def update_entry(id):
     return redirect(url_for('show_entries'))
 
 @app.route('/entries/<int:id>/delete', methods=['POST'])
+@login_required
 def delete_entry(id):
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
     entry = Entry.query.get(id)
     db.session.delete(entry)
     db.session.commit()
--- shinhon/application/flask_blog/views/views.py
+++ shinhon/application/flask_blog/views/views.py
@@ -1,12 +1,16 @@
 from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
+from functools import wraps
 
-# @app.route('/')
-# def show_entries():
-#     print(session.get('logged_in'))
-#     if not session.get('logged_in'):
-#         return redirect(url_for('login'))
-#     return render_template('entries/index.html')
+def login_required(view):
+    @wraps(view)
+    def inner(*args, **kwargs):
+        if not session.get("logged_in"):
+            return redirect(url_for("login"))
+        print(f"args: {args}, kwargs: {kwargs}")
+        print(f"Calling {view.__name__}")
+        return view(*args, **kwargs)
+    return inner
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
