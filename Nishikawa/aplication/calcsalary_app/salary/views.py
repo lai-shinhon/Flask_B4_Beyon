6e4f12e101ffc7ad86bb01db47a66eb41a4c4069
NishikawaMisaki <nishikawa.misaki.mu@mynavi.jp>
2024-05-31 00:34:48 +0000

    5/29

--- Nishikawa/aplication/calcsalary_app/salary/views.py
+++ Nishikawa/aplication/calcsalary_app/salary/views.py
@@ -10,11 +10,28 @@ def show_entries():
 @app.route('/output', methods=['GET', 'POST'])
 def outputSalary():
         
-        input_salary = int(request.form["salary"])
-        if input_salary <= 1000000:
-            tax=round(input_salary*0.1)
-        else:
-            tax=round((input_salary-1000000)*0.2+100000)
+        salary_output = None
+
+        if request.form["salary"] != "":
+              
+              #formに入力されている数値を取得
+              input_salary = int(request.form["salary"])
+              print(f"入力した数値:{input_salary}")
+              
+              #税金を保存するための変数を宣言
+              tax=0
+
+              #sessionに入力した数値を保存する
+              session["input_data"]=input_salary
+
+              if request.method == "POST":
+                    if input_salary > 1000000:
+                          #100万円を超えるときの税金を計算
+                          tax = round((input_salary-1000000)*0.2 + 1000000*0.1)
+                    else:
+                          #100万円以下の税金を計算する
+                          tax = round(input_salary*0.1)
+              salary_output = f"給与":
 
         pay=input_salary-tax
 
